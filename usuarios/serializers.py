47a1ea1eb617224afdb3e7362b5510e8838cb8d6
Felipe <110981401+felipemarra15@users.noreply.github.com>
2025-10-31 12:29:02 -0300

    me quiero morir

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -4,4 +4,4 @@ from .models import Usuario
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Usuario
-        fields = '__all__'
+        fields = ['id', 'nombre', 'email', 'telefono']
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from rest_framework import viewsets
 from .models import Usuario
 from .serializers import UsuarioSerializer
@@ -9,4 +5,3 @@ from .serializers import UsuarioSerializer
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
-
